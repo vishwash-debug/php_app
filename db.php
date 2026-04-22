ef64f3103c95fd69ccf655614eb0dc009a21337d
vishwash-debug <prazwalvishwash@gmail.com>
2026-04-22 23:41:14 +0530

    Remove unused user insertion logic
    Removed unused POST request handling for user insertion.

--- db.php
+++ db.php
@@ -1,5 +1,4 @@
 <?php
-
 $host = "database-2.ccneo2ee69pt.us-east-1.rds.amazonaws.com";
 $user = "admin";
 $password = "Vishwash";
@@ -10,11 +9,4 @@ $conn = new mysqli($host, $user, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $sql = "INSERT INTO users (name) VALUES ('$name')";
-    $conn->query($sql);
-}
-
 ?>
