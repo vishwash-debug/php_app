8de234efea9322ca937ba0b3f46f64725b00348b
vishwash-debug <prazwalvishwash@gmail.com>
2026-04-22 23:41:41 +0530

    Add user insertion logic to index.php

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
-<?php include 'db.php'; ?>
+<?php
+include 'db.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $sql = "INSERT INTO users (name) VALUES ('$name')";
+    $conn->query($sql);
+}
+?>
 
 <!DOCTYPE html>
 <html>
+<head>
+    <title>PHP App</title>
+</head>
 <body>
-
-<h2>Enter Name</h2>
-
-<form method="POST">
-  <input type="text" name="name" required>
-  <button type="submit">Submit</button>
-</form>
-
+    <h2>Enter Name</h2>
+    <form method="POST">
+        <input type="text" name="name" required>
+        <button type="submit">Submit</button>
+    </form>
 </body>
 </html>
