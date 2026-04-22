ab526310407c3714b9e97befea99048a5626a419
vishwash-debug <prazwalvishwash@gmail.com>
2026-04-22 18:10:53 +0530

    Add HTML form to index.php for name input

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
+<?php include 'db.php'; ?>
 
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Enter Name</h2>
+
+<form method="POST">
+  <input type="text" name="name" required>
+  <button type="submit">Submit</button>
+</form>
+
+</body>
+</html>
