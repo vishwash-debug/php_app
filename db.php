5b3fed74af6f43512f42aedfc35e5a3e3c06eb25
vishwash-debug <prazwalvishwash@gmail.com>
2026-04-22 18:13:09 +0530

    Add database connection and user insertion logic

--- db.php
+++ db.php
@@ -1,1 +1,20 @@
+<?php
 
+$host = "YOUR-RDS-ENDPOINT";
+$user = "admin";
+$password = "Vishwash";
+$dbname = "mydb";
+
+$conn = new mysqli($host, $user, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $sql = "INSERT INTO users (name) VALUES ('$name')";
+    $conn->query($sql);
+}
+
+?>
